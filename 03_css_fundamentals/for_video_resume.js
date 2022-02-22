160276f07350bc351938c90feef6053997d1044e
Aryan Agrawal <alemantrixaryan@gmail.com>
2022-02-22 12:00:49 +0530

    now i have the fundamentals

---
+++ 03_css_fundamentals/for_video_resume.js
@@ -0,0 +1,11 @@
+function is_Potential_employee(answer) {
+  if (answer === "yes") {
+    phone("+91 93348 01895");
+    email("alemantrixaryan@gmail.com");
+    instagram("@alemantrixaryanagrawal");
+    console.log("Thanks For Contacting me");
+  } else {
+    console.log("Thanks For Your Time");
+    console.log("Let Me Know How can I Improve Myself For Your Company");
+  }
+}
