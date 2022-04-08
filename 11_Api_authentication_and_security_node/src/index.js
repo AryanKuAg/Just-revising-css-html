95dd13f20f533c31e640d8235adb8c88f386e533
Aryan Agrawal <alemantrixaryan@gmail.com>
2022-04-09 03:15:47 +0530

    authentication started

---
+++ 11_Api_authentication_and_security_node/src/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+require("./db/mongoose");
+const userRouter = require("./routers/user");
+const taskRouter = require("./routers/task");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
+
+app.listen(port, () => {
+  console.log("Server is up on port " + port);
+});
+
+const bcrypt = require("bcryptjs");
+
+const myFunction = async () => {
+  const password = "Red12345!";
+  const hashedPassword = await bcrypt.hash(password, 8);
+
+  console.log(password);
+  console.log(hashedPassword);
+
+  const isMatch = await bcrypt.compare("red12345!", hashedPassword);
+  console.log(isMatch);
+};
+
+myFunction();
